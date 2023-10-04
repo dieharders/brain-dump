81344b67f7a585242afe7a30b45c5fcf3bc5b9f3
DIEHARDERS <cybrobo@gmail.com>
2023-10-04 01:17:15 -0700

    Add client library for homebrew api

--- components/chat-container.tsx
+++ components/chat-container.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react'
 import { type Message } from 'ai/react'
 import { LocalChat } from '@/components/local-chat'
 import { CloudChat } from '@/components/cloud-chat'
+import { useHomebrew } from '@/lib/homebrew'
 import { useSettings } from '@/components/features/settings/hooks'
 import { ModelID } from '@/components/features/settings/types'
 import { AIModels } from '@/components/features/settings/hooks'
@@ -18,50 +19,27 @@ export const ChatContainer = ({ id, initialMessages }: IProps) => {
   const { provider: selectedProvider, model: selectedModel } = useSettings()
   const [modelId, setModelId] = useState<AIModels | undefined>(undefined)
   const [connectOnce, setConnectOnce] = useState(false)
+  const { connect: connectToLocalProvider, apis } = useHomebrew()
 
   // Get model from somewhere (extension, localStorage, etc)
   useEffect(() => {
-    // This specifically works only with `local.ai` provider.
-    const getModel = async (): Promise<{ id: string }> => {
-      const ip = 'http://localhost:8000/model'
-      // We send an example prompt to get the model id
-      const options = {
-        stream: true,
-        prompt: 'Who created you?',
-        messages: [{ role: 'user', content: 'Who created you?' }],
-        temperature: 1.0,
-      }
-
-      try {
-        const response = await fetch(ip, {
-          method: 'POST',
-          mode: 'cors', // no-cors, *cors, same-origin
-          cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: 'same-origin',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          redirect: 'follow',
-          referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-          body: JSON.stringify(options),
-        })
-
-        return await response.json()
-      } catch (error) {
-        console.log('@@ error:', error)
-        toast.error('Local provider connection failed. ' + `${error}`)
-        return { id: '' }
-      }
-    }
-
     const connect = async () => {
       // Attempt to verify the local provider exists.
       if (selectedProvider === ModelID.Local) {
-        const inference = await getModel() // returns name of the inference provider's server
-        if (inference.id === 'local.ai') {
-          // console.log('@@ Connected to:', inference, 'server!')
-          toast.success(`Connected to ${inference.id}`)
+        const res = await connectToLocalProvider()
+        if (res?.success) {
+          toast.success(`<-Connected to local provider->`)
           setModelId(selectedProvider as AIModels)
+          // @TODO Temp, test completions here...
+          await apis?.['text-inference'].completions({
+            prompt: '\n\n### Instructions:\nWhat is the capital of France?\n\n### Response:\n',
+            stop: [
+              '\n',
+              '###'
+            ],
+            // temperature: 1.0,
+            // role: 'user'
+          })
         }
       } else {
         // Otherwise we are using cloud providers
@@ -75,7 +53,7 @@ export const ChatContainer = ({ id, initialMessages }: IProps) => {
       connect()
       setConnectOnce(true)
     }
-  }, [selectedModel, selectedProvider, connectOnce])
+  }, [selectedModel, selectedProvider, connectOnce, connectToLocalProvider, apis])
 
   if (
     selectedProvider === 'no provider selected' ||
---
+++ lib/homebrew.ts
@@ -0,0 +1,159 @@
+import { useState } from 'react'
+
+interface I_Endpoint {
+  name: string
+  urlPath: string
+  method: string
+}
+
+interface I_API {
+  name: string
+  port: number
+  baseUrl: string
+  endpoints: Array<I_Endpoint>
+}
+
+interface I_ServicesResponse {
+  success: boolean
+  message: string
+  data: Array<I_API>
+}
+
+interface I_ConnectResponse {
+  success: boolean
+  message: string
+  data: { docs: string }
+}
+
+type T_APIRequest = (props: any) => Promise<any | null>
+
+interface I_ServiceApis {
+  'text-inference': {
+    completions: T_APIRequest
+    embeddings: T_APIRequest
+    'chat-completions': T_APIRequest
+    models: T_APIRequest
+  }
+}
+
+const PORT = 8008
+
+const fetchConnect = async (): Promise<I_ConnectResponse | null> => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+
+  try {
+    const res = await fetch(`http://0.0.0.0:${PORT}/v1/connect`, options)
+    if (!res.ok) throw new Error(`[homebrew] HTTP error! Status: ${res.status}`)
+    return res.json()
+  } catch (err) {
+    console.log('[homebrew] connectToServer error:', err)
+    return null
+  }
+}
+
+const fetchAPIConfig = async (): Promise<I_ServicesResponse | null> => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }
+
+  try {
+    // Maybe this api url could come from the /connect ?
+    const res = await fetch(`http://0.0.0.0:${PORT}/v1/services/api`, options)
+    if (!res.ok) throw new Error(`[homebrew] HTTP error! Status: ${res.status}`)
+    return res.json()
+  } catch (err) {
+    console.log('[homebrew] fetchAPIConfig error:', err)
+    return null
+  }
+}
+
+export const connectToLocalProvider = async (): Promise<I_ConnectResponse | null> => {
+  const conn = await fetchConnect()
+  console.log('[homebrew] Connecting:', conn)
+
+  const connected = conn?.success
+  if (!connected) return null
+
+  console.log(`[homebrew] Connected to local ai engine: ${conn.message}`)
+  return conn
+}
+
+export const getAPIConfig = async () => {
+  const config = await fetchAPIConfig()
+  console.log('[homebrew] getAPIConfig:', config)
+
+  const success = config?.success
+  if (!success) return null
+
+  const apis = config.data
+  return apis
+}
+
+/**
+ * Hook for Homebrew api that handles state and connections.
+ */
+export const useHomebrew = () => {
+  const [apis, setAPI] = useState<I_ServiceApis | null>()
+
+  const connect = async () => {
+    const result = await connectToLocalProvider()
+    if (!result?.success) return null
+
+    await getServices()
+    return result
+  }
+
+  const getServices = async () => {
+    const res = await getAPIConfig()
+    const serviceApis: any = {}
+    res?.forEach(api => {
+      const origin = `${api.baseUrl}${api.port}`
+      const apiName = api.name
+      const endpoints: any = {}
+      // Parse endpoint urls
+      api.endpoints.forEach(endpoint => {
+        const url = `${origin}${endpoint.urlPath}`
+        const method = endpoint.method
+        const request = async (args: any): Promise<any | null> => {
+          try {
+            const res = await fetch(url, {
+              method,
+              mode: 'cors', // no-cors, *, cors, same-origin
+              cache: 'no-cache',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              redirect: 'follow',
+              referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+              body: JSON.stringify(args),
+            })
+            if (!res.ok)
+              throw new Error(
+                `[homebrew] ${endpoint.name} HTTP error! Status: ${res.status}`,
+              )
+            return res.json()
+          } catch (err) {
+            console.log(`[homebrew] ${endpoint.name} error:`, err)
+            return null
+          }
+        }
+
+        endpoints[endpoint.name] = request
+      })
+      // Set callbacks
+      serviceApis[apiName] = endpoints
+    })
+    setAPI(serviceApis)
+    return
+  }
+
+  return { connect, getServices, apis }
+}
