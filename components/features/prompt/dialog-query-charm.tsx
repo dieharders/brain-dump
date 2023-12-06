1e82f858917c9108812cddf55cf802c711ce2c5b
Sorob Raissi <cybrobo@gmail.com>
2023-12-05 20:36:23 -0800

    Feature/HBAPP-63: Animated charm button on click and other charm edits (#17)
    * HBAPP-63 Animate prompt menu button when clicked
    
    * HBAPP-63 Crop collections selection list height.
    Checkboxes get checked when clicking card.
    Fixed click issue with collection card.
    Change style of charms.

--- components/features/prompt/dialog-query-charm.tsx
+++ components/features/prompt/dialog-query-charm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { Dispatch, MutableRefObject, SetStateAction, useEffect, useRef, useState } from 'react'
 import {
   Dialog,
   DialogContent,
@@ -24,55 +24,80 @@ interface I_Props {
   fetchListAction: () => Promise<I_Collection[]>
   onSubmit: (charm: I_Charm) => void
   removeCharm: (id: T_CharmId) => void
+  selected: MutableRefObject<string[]> // Tracks currently selected collections
 }
 
 // A menu to select from a list of collections
 export const QueryCharmMenu = (props: I_Props) => {
   const charmId: T_CharmId = 'memory'
-  const { fetchListAction, dialogOpen, setDialogOpen, removeCharm, onSubmit } = props
+  const { selected, fetchListAction, dialogOpen, setDialogOpen, removeCharm, onSubmit } = props
   const [disableForm, setDisableForm] = useState(false)
   const [collections, setCollections] = useState<I_Collection[]>([])
-  const renderDefaultMsg = <div className="font-semibold">You have not added any collections yet.</div>
-  const [selectedMemories, setSelectedMemories] = useState<string[]>([])
+  const checkboxCallbacks = useRef<{ [key: string]: Dispatch<SetStateAction<boolean>> }>({}) // Func to check/uncheck
+  const checkboxes = useRef<string[]>([]) // Tracks all checkbox states
+  const renderDefaultMsg = <div className="font-semibold">No collections added yet.</div>
+
+  const CollectionItem = ({ item, index }: { item: I_Collection, index: number }) => {
+    const itemName = item.name
+    const [isChecked, setIsChecked] = useState<boolean>(false)
+    const [doOnce, setDoOnce] = useState(false)
+
+    const onChange = () => setIsChecked((c) => {
+      const newResult = !c
+      // Set checked list state
+      if (newResult) checkboxes.current = [...checkboxes.current, itemName]
+      else {
+        const indItem = checkboxes.current.findIndex(i => i === itemName)
+        checkboxes.current.splice(indItem, 1)
+      }
+      // Set checkbox state
+      return newResult
+    })
+
+    // Set checkboxes from parent state
+    useEffect(() => {
+      if (!doOnce && dialogOpen) {
+        setIsChecked(selected.current.includes(itemName))
+        setDoOnce(true)
+      }
+    }, [doOnce, itemName])
+
+    // Record all item's checkbox callback funcs
+    useEffect(() => {
+      checkboxCallbacks.current[itemName] = setIsChecked
+    }, [itemName])
+
+    return (
+      <span key={item.id} className="flex flex-row items-center space-x-8">
+        <Root
+          id={`c${index}`}
+          checked={isChecked}
+          onCheckedChange={onChange}
+          className="flex h-6 w-6 items-center justify-center rounded border border-gray-800 bg-black hover:bg-gray-900 hover:shadow-[0_0_0.5rem_0.1rem_rgba(99,102,241,0.9)]"
+        >
+          <Indicator>
+            <CheckIcon className="h-4 w-4" />
+          </Indicator>
+        </Root>
+        <label className="w-full flex-1" htmlFor={`c${index}`}>
+          <CollectionCard
+            collection={item}
+            onClick={onChange}
+          />
+        </label>
+      </span>
+    )
+  }
 
   const CollectionsList = ({ list }: { list: I_Collection[] }) => {
-    const items = list.map((item, i) => {
-      const itemName = item.name
-      const isChecked = selectedMemories.includes(itemName)
-      const setChecked = () => {
-        // add item
-        if (!isChecked) setSelectedMemories([...selectedMemories, itemName])
-        // remove item from list
-        else {
-          const index = selectedMemories.indexOf(itemName)
-          const newList = [...selectedMemories]
-          newList.splice(index, 1)
-          setSelectedMemories(newList)
-        }
-      }
-
-      // Render collection cards
-      return (
-        <span key={item.id} className="flex flex-row items-center space-x-8" >
-          <Root onCheckedChange={setChecked} checked={isChecked} className="flex h-6 w-6 items-center justify-center rounded border border-gray-800 bg-black hover:bg-gray-900 hover:shadow-[0_0_0.5rem_0.1rem_rgba(99,102,241,0.9)]" id={`c${i}`}>
-            <Indicator>
-              <CheckIcon className="h-4 w-4" />
-            </Indicator>
-          </Root>
-          <label className="w-full flex-1" htmlFor={`c${i}`}>
-            <CollectionCard collection={item} />
-          </label>
-        </span>
-      )
-    })
     // List Container
-    return <div className="flex w-full flex-col space-y-2 overflow-y-auto overflow-x-hidden pl-2">
-      {items}
+    return <div className="flex max-h-[32rem] w-full flex-col space-y-2 overflow-y-auto overflow-x-hidden pl-2 pr-4">
+      {list.map((item, i) => <CollectionItem key={item.id} item={item} index={i} />)}
     </div>
   }
 
   const onPromptCallback = (inputPrompt: string) => {
-    const mentionsArr = selectedMemories.map(name => `@${name}`)
+    const mentionsArr = selected.current.map(name => `@${name}`)
     const mentions = mentionsArr.join(' ')
     // We want to find all current @mentions and overwrite them
     const regexPattern = /^@\w+(\s+@\w+)*$/
@@ -102,33 +127,37 @@ export const QueryCharmMenu = (props: I_Props) => {
           </DialogDescription>
         </DialogHeader>
 
-        <Button
-          disabled={disableForm}
-          className="m-0 mt-4 w-full p-0"
-          onClick={async () => {
-            // Add all collections to list
-            setSelectedMemories([...collections.map(i => i.name)])
-            setDisableForm(false)
-          }}
-        >
-          {disableForm && <IconSpinner className="mr-2 animate-spin" />}
-          Add all
-        </Button>
-        <Button
-          disabled={disableForm}
-          className="m-0 w-full p-0"
-          onClick={async () => {
-            // Remove ourselves from active charm list
-            removeCharm(charmId)
-            // Reset state
-            setSelectedMemories([])
-            setDisableForm(false)
-            setDialogOpen(false)
-          }}
-        >
-          {disableForm && <IconSpinner className="mr-2 animate-spin" />}
-          Remove all
-        </Button>
+        {/* Header buttons */}
+        <div className="flex w-full flex-row items-center justify-between space-x-4">
+          <Button
+            disabled={disableForm}
+            className="m-0 w-full p-0"
+            onClick={async () => {
+              // Add all collections to list
+              checkboxes.current = [...collections.map(i => i.name)]
+              // Set all checkbox states
+              Object.values(checkboxCallbacks.current).forEach(check => check(true))
+              setDisableForm(false)
+            }}
+          >
+            {disableForm && <IconSpinner className="mr-2 animate-spin" />}
+            Add all
+          </Button>
+          <Button
+            disabled={disableForm}
+            className="m-0 w-full p-0"
+            onClick={async () => {
+              // Remove all collections from list
+              checkboxes.current = []
+              // Set all checkbox states
+              Object.values(checkboxCallbacks.current).forEach(check => check(false))
+              setDisableForm(false)
+            }}
+          >
+            {disableForm && <IconSpinner className="mr-2 animate-spin" />}
+            Remove all
+          </Button>
+        </div>
 
         <Separator className="my-4" />
 
@@ -144,6 +173,8 @@ export const QueryCharmMenu = (props: I_Props) => {
             variant="ghost"
             className="w-full sm:w-32"
             onClick={() => {
+              // Reset list
+              checkboxes.current = []
               setDialogOpen(false)
               setDisableForm(false)
             }}
@@ -156,18 +187,22 @@ export const QueryCharmMenu = (props: I_Props) => {
             onClick={async () => {
               setDisableForm(true)
               // Only save if we selected something
-              if (selectedMemories.length) {
+              if (checkboxes.current.length) {
+                // Save the list based on current checkboxes
+                selected.current = [...checkboxes.current]
+                // Make a charm struct
                 const charm = {
                   id: charmId,
                   toolTipText: onPromptCallback(''),
                   onPromptCallback,
                 }
+                // Add the charm to prompt
                 onSubmit(charm)
               } else {
                 // Remove ourselves from active charm list
                 removeCharm(charmId)
                 // Reset state
-                setSelectedMemories([])
+                selected.current = []
               }
               setDialogOpen(false)
               setDisableForm(false)
--- components/features/prompt/prompt-charm-button.tsx
+++ components/features/prompt/prompt-charm-button.tsx
@@ -1,21 +1,26 @@
+import { MouseEvent } from 'react'
 import { cn } from '@/lib/utils'
 import { buttonVariants } from '@/components/ui/button'
 import { IconPlus } from '@/components/ui/icons'
 import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/ui/tooltip'
-import { MouseEvent } from 'react'
 
 interface I_Props {
   onClick: (event: MouseEvent) => void
+  open: boolean
 }
 
-export const CharmMenuButton = ({ onClick }: I_Props) => {
+export const CharmMenuButton = ({ open, onClick }: I_Props) => {
+  const animStyle = open ? 'animate-pulse' : ''
+  const focusStyle = open ? 'shadow-[0_0_0.75rem_0.25rem_rgba(99,102,241,0.9)] outline-none ring-2 ring-purple-300' : ''
+  const rotateStyle = open ? 'rotate-45' : 'rotate-0'
+
   return (
     <Tooltip delayDuration={350}>
       <TooltipTrigger
         onClick={onClick}
         className={cn(
           buttonVariants({ size: 'sm', variant: 'outline' }),
-          'absolute left-0 top-4 h-8 w-8 rounded-full bg-background p-0 sm:left-4',
+          `absolute left-0 top-4 h-8 w-8 rounded-full bg-background p-0 transition ease-in-out sm:left-4 ${focusStyle} ${animStyle} ${rotateStyle}`,
         )}
       >
         <IconPlus className="text-foreground" />
