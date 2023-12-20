f4b123156b2317c99a4846f167d41d82b4e654cb
DIEHARDERS <cybrobo@gmail.com>
2023-12-20 14:05:06 -0800

    HBAPP-70 Create state objects for response menu

--- components/features/prompt/dialog-response-charm.tsx
+++ components/features/prompt/dialog-response-charm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { I_Charm } from '@/components/features/prompt/prompt-charm-menu'
+import { ChangeEvent, useState } from 'react'
 import {
   Dialog,
   DialogContent,
@@ -9,7 +9,8 @@ import {
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
-import ToggleGroup from '@/components/ui/toggle-group'
+import { I_Charm } from '@/components/features/prompt/prompt-charm-menu'
+import ToggleGroup, { T_ConvoTypes } from '@/components/ui/toggle-group'
 import { Separator } from '@/components/ui/separator'
 import { Switch } from '@/components/ui/switch'
 import { Input } from '@/components/ui/input'
@@ -26,11 +27,48 @@ interface I_Props {
   onSubmit: (charm: I_Charm) => void
 }
 
+interface I_State {
+  // Presets
+  preset: T_ConvoTypes
+  // Advanced
+  n_ctx: number | string
+  seed: number | string
+  n_threads: number | string
+  n_batch: number | string
+  f16_kv: boolean
+  use_mlock: boolean
+}
+
 export const ResponseCharmMenu = (props: I_Props) => {
   const { dialogOpen, setDialogOpen } = props
   const infoClass = "flex w-full flex-row gap-2"
   const inputContainerClass = "grid w-full gap-1"
-
+  const toggleGroupClass = "flex flex-row gap-2 rounded p-2"
+  // State values
+  const defaultState: I_State = {
+    preset: 'conversational',
+    n_ctx: 512,
+    seed: 1337,
+    n_threads: -1,
+    n_batch: 512,
+    f16_kv: true,
+    use_mlock: false,
+  }
+  // @TODO Pass in from persistent storage (upon menu open) and assign here
+  const [state, setState] = useState<I_State>({
+    n_ctx: '',
+    seed: '',
+    n_threads: '',
+    n_batch: '',
+    f16_kv: defaultState.f16_kv,
+    use_mlock: defaultState.use_mlock,
+    preset: defaultState.preset,
+  })
+  // Handle input state changes
+  const handleFloatChange = (propName: string, event: ChangeEvent<HTMLInputElement>) => setState(prev => ({ ...prev, [propName]: parseFloat(event.target.value) }))
+  const handleBoolChange = (propName: string, value: boolean) => setState(prev => ({ ...prev, [propName]: value }))
+  const handleStringChange = (propName: string, value: string) => setState(prev => ({ ...prev, [propName]: value }))
+  // Menus
   const presetsMenu = (
     <>
       <DialogHeader className="my-8">
@@ -42,22 +80,27 @@ export const ResponseCharmMenu = (props: I_Props) => {
 
       {/* Content */}
       <div className="w-full">
-        <ToggleGroup label="Response Type" defaultValue="1">
+        <ToggleGroup
+          label="Response Type"
+          defaultValue={defaultState.preset}
+          value={state.preset}
+          onChange={val => handleStringChange('preset', val)}
+        >
           {/* Q and A */}
-          <>
+          <div id="qa" className={toggleGroupClass}>
             <QuestionMarkIcon className="h-10 w-10 self-center rounded-sm bg-background p-2" />
             <span className="flex-1 self-center text-ellipsis">Question & Answer</span>
-          </>
+          </div>
           {/* Conversational */}
-          <>
+          <div id="conversational" className={toggleGroupClass}>
             <IconConversationType className="h-10 w-10 self-center rounded-sm bg-background p-2" />
             <span className="flex-1 self-center text-ellipsis">Conversational</span>
-          </>
+          </div>
           {/* Assistant */}
-          <>
+          <div id="assistant" className={toggleGroupClass}>
             <PersonIcon className="h-10 w-10 self-center rounded-sm bg-background p-2" />
             <span className="flex-1 self-center text-ellipsis">Assistant</span>
-          </>
+          </div>
         </ToggleGroup>
       </div>
 
@@ -87,85 +130,107 @@ export const ResponseCharmMenu = (props: I_Props) => {
         <div className={inputContainerClass}>
           <div className={infoClass}>
             <Label className="text-sm font-semibold">Context Size</Label>
-            <Info label="n_ctx"><span><Highlight>n_ctx</Highlight> determines the number of tokens to consider when generating a response.</span></Info>
+            <Info label="n_ctx">
+              <span><Highlight>n_ctx</Highlight> determines the number of tokens to consider when generating a response.</span>
+            </Info>
           </div>
           <Input
             name="url"
             type="number"
-            // value={512}
+            value={state.n_ctx}
             min={64}
             step={1}
-            placeholder="512"
+            placeholder={defaultState.n_ctx.toString()}
             className="w-full"
-            onChange={() => { }}
+            onChange={event => handleFloatChange('n_ctx', event)}
           />
         </div>
         {/* Seed */}
         <div className={inputContainerClass}>
           <div className={infoClass}>
             <Label className="text-sm font-semibold">Seed</Label>
-            <Info label="seed"><span><Highlight>seed</Highlight> determines the randomness seed. 0 for different seed on every prompt.</span></Info>
+            <Info label="seed">
+              <span><Highlight>seed</Highlight> 0 for non-deterministic randomness for each response. Non 0 for deterministic.</span>
+            </Info>
           </div>
           <Input
             name="url"
             type="number"
-            // value={1337}
+            value={state.seed}
             min={0}
             step={1}
-            placeholder="1337"
+            placeholder={defaultState.seed.toString()}
             className="w-full"
-            onChange={() => { }}
+            onChange={event => handleFloatChange('seed', event)}
           />
         </div>
         {/* Number of threads (n_threads) */}
         <div className={inputContainerClass}>
           <div className={infoClass}>
             <Label className="text-sm font-semibold"># Threads</Label>
-            <Info label="n_threads"><span><Highlight>n_threads</Highlight> number of GPU threads to use when generating. If None, the number is automatically determined.</span></Info>
+            <Info label="n_threads">
+              <span><Highlight>n_threads</Highlight> number of GPU threads to use when generating. If None, the number is automatically determined.</span>
+            </Info>
           </div>
           <Input
             name="url"
             type="number"
-            // value={-1}
+            value={state.n_threads}
             min={-1}
             step={1}
-            placeholder="-1"
+            placeholder={defaultState.n_threads.toString()}
             className="w-full"
-            onChange={() => { }}
+            onChange={event => handleFloatChange('n_threads', event)}
           />
         </div>
         {/* Max Batch Number (n_batch) - Maximum number of prompt tokens to batch together when calling llama_eval */}
         <div className={inputContainerClass}>
           <div className={infoClass}>
             <Label className="text-sm font-semibold">Batch Token Size</Label>
-            <Info label="n_batch"><span><Highlight>n_batch</Highlight> Maximum number of prompt tokens to batch together when generating.</span></Info>
+            <Info label="n_batch">
+              <span><Highlight>n_batch</Highlight> Maximum number of prompt tokens to batch together when generating.</span>
+            </Info>
           </div>
           <Input
             name="url"
             type="number"
-            // value={512}
+            value={state.n_batch}
             min={64}
             step={1}
-            placeholder="512"
+            placeholder={defaultState.n_batch.toString()}
             className="w-full"
-            onChange={() => { }}
+            onChange={event => handleFloatChange('n_batch', event)}
           />
         </div>
         {/* Toggle precision (f16_kv) */}
         <div className={inputContainerClass}>
           <div className={infoClass}>
             <Label className="text-sm font-semibold">Half-Precision</Label>
-            <Info label="f16_kv"><span><Highlight>f16_kv</Highlight> Use half-precision for key/value generation cache.</span></Info>
+            <Info label="f16_kv">
+              <span><Highlight>f16_kv</Highlight> Use half-precision for key/value generation cache.</span>
+            </Info>
           </div>
-          <Switch className="block" defaultChecked />
+          <Switch
+            className="block"
+            defaultChecked={defaultState.f16_kv}
+            checked={state.f16_kv}
+            onCheckedChange={val => handleBoolChange('f16_kv', val)}
+          />
         </div>
         {/* Memory Lock (use_mlock) */}
         <div className={inputContainerClass}>
           <div className={infoClass}>
             <Label className="text-sm font-semibold">Memory Lock</Label>
-            <Info label="use_mlock"><span><Highlight>use_mlock</Highlight> forces the system to keep the model in RAM.</span></Info>
+            <Info label="use_mlock">
+              <span><Highlight>use_mlock</Highlight> forces the system to keep the model in RAM.</span>
+            </Info>
           </div>
-          <Switch className="block" />
+          <Switch
+            className="block"
+            defaultChecked={defaultState.use_mlock}
+            checked={state.use_mlock}
+            onCheckedChange={val => handleBoolChange('use_mlock', val)}
+          />
         </div>
       </form>
 
--- components/ui/toggle-group.tsx
+++ components/ui/toggle-group.tsx
@@ -1,31 +1,50 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Root, Item } from '@radix-ui/react-toggle-group'
+
+export type T_ConvoTypes = 'qa' | 'conversational' | 'assistant'
 
 interface I_Props {
   children: React.ReactNode
   label: string
-  defaultValue: string
+  defaultValue: T_ConvoTypes
+  value: T_ConvoTypes
+  onChange: (val: T_ConvoTypes) => void
 }
 
-const ToggleGroup = ({ children, label = 'Toggle Group', defaultValue = '0' }: I_Props) => {
-  const [checked, setChecked] = useState<string>(defaultValue)
-
+const ToggleGroup = ({ children, label = 'Toggle Group', defaultValue = 'conversational', value, onChange }: I_Props) => {
   return (
     <Root
       className="border-1 flex w-fit flex-row flex-wrap gap-2 rounded-sm border-solid border-black bg-muted p-2"
       type="single" // assign unique "name" prop to each <input> if you wish to use type="multiple"
       defaultValue={defaultValue}
       aria-label={label}
-      onValueChange={val => val && setChecked(val)}
+      onValueChange={(val: T_ConvoTypes) => val && onChange(val)}
     >
-      {React.Children.map(children, (child, index) => (
-        <Item className="h-auto w-fit flex-1 rounded text-left hover:bg-primary/10" value={index.toString()} aria-label={`${label} ${index.toString()}`}>
-          <input className="peer hidden" type="radio" checked={checked === index.toString()} onChange={() => { /* Prevent browser error msg */ }} name="toggle-radio-button" />
-          <div className="flex flex-row gap-2 rounded p-2 peer-checked:bg-primary/25" onClick={() => { /* Save state here */ }}>
+      {React.Children.map(children, (child, index) => {
+        // Children should have "id" attr set on the parent,
+        // otherwise their index is used as the value
+        const id: string = React.isValidElement(child) && child.props.id
+        const val = id || index.toString()
+        const isChecked = value === id || value === index.toString()
+        const focusClass = isChecked ? 'bg-primary/25' : ''
+
+        return (
+          <Item
+            value={val}
+            aria-label={`${label} ${index.toString()}`}
+            className={`h-auto w-fit flex-1 rounded text-left hover:bg-primary/10 ${focusClass}`}
+          >
+            <input
+              name="toggle-radio-button"
+              type="radio"
+              checked={isChecked}
+              onChange={() => { /* Prevent browser error msg */ }}
+              className="peer hidden"
+            />
             {child}
-          </div>
-        </Item>
-      ))}
+          </Item>
+        )
+      })}
 
     </Root>
   )
