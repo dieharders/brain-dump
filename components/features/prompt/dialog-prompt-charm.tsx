74fe820a32f86f33d434173332747a232c80e7a7
DIEHARDERS <cybrobo@gmail.com>
2024-01-27 12:19:56 -0800

    HBAPP-86 Add retrieval ui inputs.

--- components/features/prompt/dialog-prompt-charm.tsx
+++ components/features/prompt/dialog-prompt-charm.tsx
@@ -55,6 +55,7 @@ export const PromptTemplateCharmMenu = (props: I_Props) => {
   const [ragPromptSource, setRagPromptSource] = useState<string>() // llama-index prompts
   const infoClass = "flex w-full flex-row gap-2"
   const inputContainerClass = "grid w-full gap-1"
+
   // Default state values
   const defaultState = useMemo(() => {
     return {
@@ -70,6 +71,7 @@ export const PromptTemplateCharmMenu = (props: I_Props) => {
       repeat_penalty: 1.1,
       stream: true,
       echo: false,
+      similarity_top_k: 1,
     }
   }, [])
 
@@ -87,6 +89,8 @@ export const PromptTemplateCharmMenu = (props: I_Props) => {
     repeat_penalty: defaultState.repeat_penalty,
     stream: defaultState.stream,
     echo: defaultState.echo,
+    similarity_top_k: defaultState.similarity_top_k,
+    response_mode: undefined,
   })
 
   const handleFloatValue = (val: any) => {
@@ -204,8 +208,19 @@ export const PromptTemplateCharmMenu = (props: I_Props) => {
     return [customGroup, ...presets]
   }, [promptTemplates?.normal_presets])
 
+  const getResponseModes = useCallback(() => {
+    const parseString = (str: string) => {
+      const words = str.split('_')
+      words.map(w => w.charAt(0).toUpperCase() + w.slice(1).toLowerCase())
+      return words.join(' ')
+    }
+    // @TODO Get from api endpoint
+    const data = ['COMPACT']
+    return data.map(i => <SelectItem key={i} value={i}>{parseString(i)}</SelectItem>)
+  }, [])
+
   const presetsMenu = (
-    <>
+    <div className="px-1">
       {/* Accuracy Presets */}
       <DialogHeader className="my-8">
         <DialogTitle>Accuracy Settings</DialogTitle>
@@ -389,11 +404,11 @@ export const PromptTemplateCharmMenu = (props: I_Props) => {
       <DialogFooter className="items-stretch">
         <Button onClick={onSave}>Save</Button>
       </DialogFooter>
-    </>
+    </div>
   )
 
   const advancedMenu = (
-    <>
+    <div className="px-1">
       {/* Advanced Settings, should override all other settings */}
       <DialogHeader className="my-8">
         <DialogTitle>Advanced Settings</DialogTitle>
@@ -402,8 +417,8 @@ export const PromptTemplateCharmMenu = (props: I_Props) => {
         </DialogDescription>
       </DialogHeader>
 
-      {/* Content */}
-      <form className="grid-auto-flow m-auto grid w-fit grid-flow-row auto-rows-max grid-cols-2 gap-4" method="POST" encType="multipart/form-data">
+      {/* Options Content */}
+      <div className="grid-auto-flow m-auto grid w-fit grid-flow-row auto-rows-max grid-cols-2 gap-4">
         {/* Temperature (temperature) */}
         <div className={inputContainerClass}>
           <div className={infoClass}>
@@ -549,14 +564,73 @@ export const PromptTemplateCharmMenu = (props: I_Props) => {
             onCheckedChange={val => handleStateChange('echo', val)}
           />
         </div>
-      </form>
+      </div>
+
+      <Separator className="my-6" />
+
+      {/* RAG Options ONLY */}
+      <DialogHeader className="my-8">
+        <DialogTitle>Memory Retrieval</DialogTitle>
+        <DialogDescription>
+          Only applies to queries that use external memories as context.
+        </DialogDescription>
+      </DialogHeader>
+
+      {/* RAG Options Content */}
+      <div className="grid-auto-flow m-auto grid w-fit grid-flow-row auto-rows-max grid-cols-2 gap-4">
+        {/* Max Number of Results (similarity_top_k) */}
+        <div className={inputContainerClass}>
+          <div className={infoClass}>
+            <Label className="text-sm font-semibold">Num Matching Results</Label>
+            <Info label="similarity_top_k">
+              <span><Highlight>similarity_top_k</Highlight> determines how many matching documents to consider when synthesizing a response.</span>
+            </Info>
+          </div>
+          <Input
+            name="url"
+            type="number"
+            value={handleFloatValue(state?.similarity_top_k)}
+            min={1}
+            step={1}
+            placeholder={defaultState?.similarity_top_k?.toString()}
+            className="w-full"
+            onChange={event => handleFloatChange('similarity_top_k', event.target.value)}
+          />
+        </div>
+
+        {/* Type of response (response_mode) */}
+        <div className={inputContainerClass}>
+          <div className={infoClass}>
+            <Label className="text-sm font-semibold">Type of Response</Label>
+            <Info label="response_mode">
+              <span><Highlight>response_mode</Highlight> determines how the LLM responds to the context.</span>
+            </Info>
+          </div>
+          <div className="w-full">
+            <Select
+              defaultValue={undefined}
+              value={state?.response_mode}
+              onValueChange={value => handleStateChange('response_mode', value)}
+            >
+              <SelectTrigger className="w-full flex-1">
+                <SelectValue placeholder="Select Response Mode"></SelectValue>
+              </SelectTrigger>
+              <SelectGroup>
+                <SelectContent className="p-1">
+                  {getResponseModes()}
+                </SelectContent>
+              </SelectGroup>
+            </Select>
+          </div>
+        </div>
+      </div>
 
       <Separator className="my-6" />
 
       <DialogFooter className="items-stretch">
         <Button onClick={onSave}>Save</Button>
       </DialogFooter>
-    </>
+    </div>
   )
 
   const tabs = [
