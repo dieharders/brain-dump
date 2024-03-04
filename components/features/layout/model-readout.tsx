6e75f59618f1c3f59a2cd0e0c06c4730a595a425
DIEHARDERS <cybrobo@gmail.com>
2024-03-04 10:02:01 -0800

    HBAPP-97 Fix model readout name

--- components/features/layout/model-readout.tsx
+++ components/features/layout/model-readout.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useRef } from 'react'
+import { useEffect, useState } from 'react'
 import { usePathname } from 'next/navigation'
 import { useHomebrew } from '@/lib/homebrew'
 import { ROUTE_CHATBOT, ROUTE_PLAYGROUND } from '@/app/constants'
@@ -8,7 +8,7 @@ import { ROUTE_CHATBOT, ROUTE_PLAYGROUND } from '@/app/constants'
 export const ModelReadout = () => {
   const { getServices } = useHomebrew()
   // @TODO Should really use something like Redux to get naem from parent and pass in
-  const title = useRef('???')
+  const [title, setTitle] = useState('???')
   // @TODO Get these values from somewhere
   const gpu = '0'
   const cpu = '0'
@@ -21,7 +21,7 @@ export const ModelReadout = () => {
       const services = await getServices()
       const model = await services?.textInference.model()
       const name = model?.data?.model_name
-      if (name) title.current = name
+      name && setTitle(name)
     }
     if (shouldRender) action()
   }, [getServices, shouldRender])
@@ -31,7 +31,7 @@ export const ModelReadout = () => {
       ?
       <div className="w-fill flex flex-1 items-center justify-center gap-4 overflow-hidden whitespace-nowrap rounded-md border-2 border-primary/10 bg-muted p-2 duration-100 ease-out hover:border-primary/20 hover:bg-primary/20">
         <div className="text-sm text-muted-foreground">GPU: {gpu}% | CPU: {cpu}%</div>
-        <div className="text-muted-foreground">Model: {title.current}</div>
+        <div className="text-muted-foreground">Model: {title}</div>
       </div>
       :
       null
