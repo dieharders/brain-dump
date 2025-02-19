3bc657ec656cf2dfc2b9c03c1b785a778f64d3ca
DIEHARDERS <cybrobo@gmail.com>
2025-02-19 13:42:11 -0800

    HBAPP-252 "Dismiss" hangs outside of toast div

--- lib/notifications.tsx
+++ lib/notifications.tsx
@@ -3,7 +3,7 @@ import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
 
 export const notifications = () => {
-  const dismissStyle = cn("self-center rounded-md border p-4")
+  const dismissStyle = cn('self-center rounded-md border p-4')
 
   const notAvailable = () =>
     toast('Pardon our dust.\nThis feature is not yet available.', { icon: '🧹💨' })
@@ -11,7 +11,7 @@ export const notifications = () => {
   const loadModel = async (promise: Promise<any>, cancel?: () => void) =>
     toast.promise(promise, {
       loading:
-        <div className={cn("flex flex-row items-center justify-center")}>
+        <div className={cn('flex flex-col flex-wrap items-center justify-center gap-4 overflow-hidden')}>
           <div className="flex flex-col gap-1 pr-2">
             <b>Loading model 🤖</b>
             <p>Please wait...</p>
@@ -25,10 +25,10 @@ export const notifications = () => {
             }}>Cancel</Button>
         </div>,
       success: (res: any) =>
-        <div className="flex flex-row items-center justify-center">
-          <span>
+        <div className="flex flex-col items-center justify-center gap-4">
+          <span className="flex flex-col gap-1">
             <b className="pr-2">Model loaded!</b>
-            <br />{res?.message}
+            <br />{res?.message} flex flex-col items-center justify-center gap-4
           </span>
           <Button
             variant="default"
@@ -38,7 +38,7 @@ export const notifications = () => {
             }}>Dismiss</Button>
         </div>,
       error: (err: any) => (
-        <div className="flex gap-2">
+        <div className="flex flex-col gap-4">
           <p>
             <b>Could not load model.</b>
             <br></br>
