dd458f188742757d66878d161a50702f245cf7fb
DIEHARDERS <cybrobo@gmail.com>
2025-03-22 09:33:14 -0700

    HBAPP-260 Edit copy

--- components/features/menus/tabs/info-template-vars.tsx
+++ components/features/menus/tabs/info-template-vars.tsx
@@ -5,7 +5,7 @@ import { Label } from '@/components/ui/label'
  * Info about all template vars used to inject extra info into template strings.
  */
 export const TemplateVarsInfo = () => {
-  const templateStringVars = '{{user_prompt}} {context_str} {tool_arguments_str} {tool_example_str} {tool_name_str} {tool_description_str} {assigned_tools_str}'
+  const templateStringVars = '{{user_prompt}} {{context_str}} {{tool_arguments_str}} {{tool_example_str}} {{tool_name_str}} {{tool_description_str}} {{assigned_tools_str}}'
 
   return (
     <div className="mb-4 flex w-full flex-row gap-2">
